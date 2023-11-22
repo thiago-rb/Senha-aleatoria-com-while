3e75aa2e476610dd9b898b04d3a1015a3e5c4e25
Thiago de Souza <thiagodesouzarb@gmail.com>
2023-11-22 20:25:56 -0300

    Senha aleatoria

---
+++ WhileExercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileExercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = 2002;
+            Console.Write("Digite a senha correta: ");
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha inválida");
+                Console.Write("Digite a senha novamente: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso permitido!");
+
+        }
+    }
+}
